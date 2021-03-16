ec1898df903da61fa91cd7b62e870313ffd6cbaa
Nimesh <prajapatin953@gmail.com>
2021-03-16 18:56:45 +0530

    added polymorphism file

--- opps/inheritance.py
+++ opps/inheritance.py
@@ -1,141 +1,221 @@
-#inheritance
-class first():
-    def prime_number(self,num):
+# inheritance
+class first:
+    def prime_number(self, num):
         if num > 1:
-	        for i in range(2,num):
-		        if (num%i)==0:
-			        print(num,"is not prime")
-			        break
-	        else:
-		        print("is prime number")
-        else:       
-	        print("not prime number")			
-
+            for i in range(2, num):
+                if (num % i) == 0:
+                    print(num, "is not prime")
+                    break
+            else:
+                print("is prime number")
+        else:
+            print("not prime number")
 
     def lastname(self):
         print("hello prajapati")
+
+
 class second(first):
     def mes_stud():
         print("second classs")
-a=second()
+
+
+a = second()
 a.prime_number(9)
 
 
-
-
-
 class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+    names = "nimu"
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
 
 class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
 
-  def welcome(self):
-    print("Welcome", self.firstname, self.lastname, "to the Botree", self.graduationyear)
+    def welcome(self):
+        print(
+            "Welcome",
+            self.firstname,
+            self.lastname,
+            "to the Botree",
+            self.graduationyear,
+        )
+
 
 x = Student("nimesh", "prajapati", 2021)
 x.welcome()
+p1 = Person("nimesh", "praja")
+p1.printname()
+p1.names
 
 
+# multilevel inheritance
 
-#multilevel inheritance
 
-class A():
-    def __init__(self,age):
-        self.sage=age
+class A:
+    def __init__(self, age):
+        self.sage = age
+
     def print_age(self):
-        print(self.sage)    
+        print(self.sage)
+
+
 class B(A):
-    def __init__(self,name):
-        self.sname=name
+    def __init__(self, name, age):
+        # self.sage=age
+        self.sname = name
+        A.__init__(self, age)
+
     def print_name(self):
         print(self.sname)
+
+
 class C(B):
-    def print_mess():
+    def print_mess(self):
         print("hello this is c class")
 
-p1=B("nimesh")
-p1.print_name()
 
-#multilevel
+t1 = C("shailesh", 25)
+t1.print_name()
+t1.print_mess()
+t1.print_age()
+
+# multilevel
 class Grandfather:
- 
     def __init__(self, grandfathername):
         self.grandfathername = grandfathername
- 
+
+
 # Intermediate class
 class Father(Grandfather):
     def __init__(self, fathername, grandfathername):
         self.fathername = fathername
- 
+
         # invoking constructor of Grandfather class
         Grandfather.__init__(self, grandfathername)
- 
+
+
 # Derived class
 class Son(Father):
-    def __init__(self,sonname, fathername, grandfathername):
+    def __init__(self, sonname, fathername, grandfathername):
         self.sonname = sonname
- 
+
         # invoking constructor of Father class
         Father.__init__(self, fathername, grandfathername)
- 
+
     def print_name(self):
-        print('Grandfather name :', self.grandfathername)
+        print("Grandfather name :", self.grandfathername)
         print("Father name :", self.fathername)
         print("Son name :", self.sonname)
- 
+
+
 #  Driver code
-s1 = Son('Prince', 'Rampal', 'Lal mani')
+s1 = Son("Prince", "Rampal", "Lal mani")
 print(s1.grandfathername)
 s1.print_name()
 
 
+# multiple inheritance
+class Class1:
+    def m(self):
+        print("In Class1")
 
-#multiple inheritance
-class Class1: 
-    def m(self): 
-        print("In Class1")    
-      
-class Class2(Class1): 
-    def m(self): 
-        print("In Class2") 
-        Class1.m(self) 
-  
-class Class3(Class1): 
-    def m(self): 
-        print("In Class3") 
-        Class1.m(self)    
-       
-class Class4(Class2, Class3): 
-    def m(self): 
-        print("In Class4")    
-        Class2.m(self) 
-        Class3.m(self) 
-       
-obj = Class4() 
-obj.m() 
 
-#overiding
+class Class2(Class1):
+    def m(self):
+        print("In Class2")
+        Class1.m(self)
+
+
+class Class3(Class1):
+    def m(self):
+        print("In Class3")
+        Class1.m(self)
+
+
+class Class4(Class2, Class3):
+    def m(self):
+        print("In Class4")
+        Class2.m(self)
+        Class3.m(self)
+
+
+obj = Class4()
+obj.m()
+
+# overiding
 class Robot:
-    
     def __init__(self, name):
         self.name = name
-        
-    def say_hi(self):
-        print("Hi, I am " + self.name)
-        
-class PhysicianRobot(Robot):
 
     def say_hi(self):
-        print("Everything will be okay! ") 
+        print("Hi, I am " + self.name)
+
+
+class PhysicianRobot(Robot):
+    def say_hi(self):
+        print("Everything will be okay! ")
         print(self.name + " takes care of you!")
+
 
 y = PhysicianRobot("James")
 y.say_hi()
+
+
+class sum:
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def add(self):
+        m3 = self.m1 + self.m2
+        print("the sum is ", m3)
+
+
+class sub(sum):
+    def __init__(self, m1, m2):
+        # self.p1=p1
+        # self.p2=p2
+        sum.__init__(self, m1, m2)
+
+    def subt(self):
+        m3 = self.m1 - self.m2
+        print("the diffrenece is", m3)
+
+
+class mul(sub):
+    def __init__(self, m1, m2):
+        sub.__init__(self, m1, m2)
+
+    def mult(self):
+        m3 = self.m1 * self.m2
+        print("multiplication", m3)
+
+
+pk = mul(5, 2)
+pk.add()
+pk.subt()
+pk.mult()
+
+
+class circle:
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def get_ans(self):
+        return self.radius * self.pi * 2
+
+
+my_circle = circle()
+print(my_circle.pi)
+print(my_circle.get_ans())
---
+++ opps/polymorphism.py
@@ -0,0 +1,207 @@
+
+#method overloading
+class fruits():
+    def __init__(self,name):
+        self.name=name
+    def print_mess(self):
+        print("hello welcome",self.name)
+    
+class flowers(fruits):
+    def __init__(self,color,types):
+        self.color=color
+        self.types=types
+    def print_mess(self):
+        print("hi",self.color)
+        print("nimesh",self.types)
+
+# obj=flowers("nimesh")
+# obj.print_mess()
+obj1=flowers("red","mast")
+obj1.print_mess()
+
+       
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.l = length
+        self.b = breadth
+    def area(self):
+        return self.l * self.b
+class Square:
+    def __init__(self, side):
+        self.s = side
+    def area(self):
+        return self.s ** 2
+rec = Rectangle(10, 20)
+squ = Square(10)
+print("Area of rectangle is: ", rec.area())
+print("Area of square is: ", squ.area())
+  
+
+# #duck typing
+class parents():
+    def viewdata(self):
+        print("this is parents class")
+        print("welcome to parents class")
+
+class subchild():
+    def viewdata(self):
+        print("hello")
+        print("welcome")
+        print("good morning")
+
+class hello():
+    def Data(self):
+        print("nimesh")
+
+class child():
+    def code(self,id):
+        id.viewdata()
+
+id=subchild()
+
+obj=child()
+obj.code(id)
+
+#overrridding
+class fruits:
+    def print_mess(self):
+        print("hello")
+        
+    
+class flowers(fruits):
+    def print_mess(self):
+        pass
+        # print("hello welcome")
+    
+p1=flowers()
+print(p1.print_mess())
+
+
+#multiple inheritance using overriding
+class Parent1(): 
+          
+    # Parent's show method 
+    def show(self): 
+        print("Inside Parent1") 
+          
+# Defining Parent class 2 
+class Parent2(): 
+          
+    # Parent's show method 
+    def display(self): 
+        print("Inside Parent2") 
+          
+          
+# Defining child class 
+class Child(Parent1, Parent2):  
+    def show(self): 
+        Parent1.show(self)
+        # print("Inside Child") 
+     
+        
+# Driver's code 
+obj = Child() 
+obj.show()
+
+
+#multilevel inheritance
+
+class Parent():  
+        
+    # Parent's show method 
+    def display(self): 
+        print("Inside Parent") 
+    
+    
+# Inherited or Sub class (Note Parent in bracket)  
+class Child(Parent):  
+        
+    # Child's show method 
+    def show(self): 
+        print("Inside Child") 
+    
+# Inherited or Sub class (Note Child in bracket)  
+class GrandChild(Child):  
+          
+    # Child's show method 
+    def show(self):
+        Child.show(self)
+        pass 
+        # print("Inside GrandChild")          
+    
+# Driver code  
+g = GrandChild()    
+g.show() 
+g.display() 
+
+
+# #multiple inheritance
+
+class one:
+    def data(self):
+        pass
+        # print("from first classs")
+class two:
+    def data(self):
+        print("from second class")
+class three(one,two):
+    def display(self):
+        two.data
+        print("third classs")
+
+m1=three()
+m1.data()
+
+
+
+#method overriding example with multilevel inheritance
+
+class odd_even():
+    def __init__(self,num):
+        self.num=num
+    def check_num(self):
+        if self.num == 0:
+            print("must be greater than zero")
+        elif self.num%2==0:
+            print(self.num," Is Even number")
+        else:
+            print(self.num," Is odd number")
+
+
+class pos_neg(odd_even):
+    def __init__(self,num):
+        self.num=num
+        odd_even.check_num(self)
+    def check_num(self):
+        if self.num > 0:
+            print(self.num,"Is Positive Number")
+        elif self.num==0:
+            print("Number is zero")
+        else:
+            print(self.num,"Is Negative Number")
+    
+class is_prime(pos_neg):
+    def __init__(self,num):
+        self.num=num
+        odd_even.check_num(self)
+    def check_num(self):
+        if (self.num == 1):
+            print("1 is neither prime nor composite")
+        elif (self.num <= 0):
+            print("Enter valid number")
+        else:
+            for number in range(2, self.num):
+                if(self.num % number == 0):
+                    print(self.num," Is not Prime Number")
+                    break
+            else:
+                print(self.num,"Is Prime Number")
+
+            
+
+obj=odd_even(4)
+obj.check_num()
+obj1=pos_neg(1)
+obj1.check_num()
+obj=is_prime(1)
+obj.check_num()
